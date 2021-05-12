57ddc1d798e105eec994062b07d78d7540345703
bekkos <bekkosm@gmail.com>
2021-05-13 00:53:27 +0200

    13.05

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, redirect
 
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
--- static/javascript/nav-logic.js
+++ static/javascript/nav-logic.js
@@ -12,20 +12,20 @@ const toggleMenu = () => {
     menuState = !menuState;
 }
 
-let updated = false;
-setInterval(() => {
-    if(screen.width > 1024) {
-        document.getElementById('bm-toggle-btn-close').style.display = "none";
-        document.getElementById('bm-toggle-btn-open').style.display = "none";
-        updated = true;
-    } else if(screen.width < 1024 && updated) {
-        document.getElementById('bm-toggle-btn-open').style.display = "inline";
-        updated = false;
-    }
+// let updated = false;
+// setInterval(() => {
+//     if(screen.width > 1024) {
+//         document.getElementById('bm-toggle-btn-close').style.display = "none";
+//         document.getElementById('bm-toggle-btn-open').style.display = "none";
+//         updated = true;
+//     } else if(screen.width < 1024 && updated) {
+//         document.getElementById('bm-toggle-btn-open').style.display = "inline";
+//         updated = false;
+//     }
 
-    if(screen.width > 1024 && document.getElementById('hb-menu').style.display == "block") {
-        document.getElementById('hb-menu').style.display = "none";
-        menuState = !menuState;
-    }
+//     if(screen.width > 1024 && document.getElementById('hb-menu').style.display == "block") {
+//         document.getElementById('hb-menu').style.display = "none";
+//         menuState = !menuState;
+//     }
     
-},5);
+// },5);
